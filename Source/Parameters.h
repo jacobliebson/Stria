bd0388039019e57b5d1c3995d5b7ce9a439bb1f9
jel212 <jel212@case.edu>
2026-06-06 14:14:58 -0400

    moved parameter tree setup to Parameters.cpp

---
+++ Source/Parameters.h
@@ -0,0 +1,6 @@
+#include <juce_audio_processors/juce_audio_processors.h>
+
+class Parameters {
+    public:
+        static juce::AudioProcessorValueTreeState::ParameterLayout configureParameters ();
+};
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Arpeggiator.h"
 #include "CombFilter.h"
+#include "Parameters.h"
 #include <juce_audio_processors/juce_audio_processors.h>
 
 class AudioPluginAudioProcessor  : public juce::AudioProcessor
